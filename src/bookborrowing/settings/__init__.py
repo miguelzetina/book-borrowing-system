2b943821e5104e01932fc9dba94a3dec5ac2c837
Miguel Angel <loefthur26@gmail.com>
2018-11-03 10:17:17 -0600

    Change settings confs

--- src/bookborrowing/settings/__init__.py
+++ src/bookborrowing/settings/__init__.py
@@ -55,7 +55,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.realpath(os.path.join(BASE_DIR, 'templates'))
+            os.path.realpath(os.path.join(BASE_DIR, '..', 'templates'))
         ],
         'APP_DIRS': True,
         'OPTIONS': {
