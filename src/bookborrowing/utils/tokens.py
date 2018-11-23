adf8ff0025c476b1d2071d38ce62234c820c9752
Miguel Angel <loefthur26@gmail.com>
2018-11-22 23:12:43 -0600

    Change util for get jwt for user

--- src/bookborrowing/utils/tokens.py
+++ src/bookborrowing/utils/tokens.py
@@ -2,43 +2,18 @@
 from calendar import timegm
 from datetime import datetime
 
+from django.utils.six import text_type
+
 from jwt import decode
-
-from rest_framework_jwt.settings import api_settings
-
-
-def jwt_decode_handler(token):
-    """
-    Decode token for user
-    """
-    options = {
-        'verify_exp': False,
-    }
-
-    return decode(
-        token,
-        api_settings.JWT_SECRET_KEY,
-        api_settings.JWT_VERIFY,
-        options=options,
-        leeway=api_settings.JWT_LEEWAY,
-        audience=api_settings.JWT_AUDIENCE,
-        issuer=api_settings.JWT_ISSUER,
-        algorithms=[api_settings.JWT_ALGORITHM]
-    )
-
+from rest_framework_simplejwt.tokens import RefreshToken
 
 def create_token(user):
     """
     Create token.
     """
-    jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-    jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
-    payload = jwt_payload_handler(user)
-    if api_settings.JWT_ALLOW_REFRESH:
-        payload['orig_iat'] = timegm(
-            datetime.utcnow().utctimetuple()
-        )
 
-    # Return value
-    token = jwt_encode_handler(payload)
+    refresh = RefreshToken.for_user(user)
+
+    token = text_type(refresh.access_token)
+
     return token
