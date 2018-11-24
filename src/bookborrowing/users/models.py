afc34c93a598f22c6191ad4e906f5e4453e7a9b9
Miguel Angel <loefthur26@gmail.com>
2018-11-23 23:31:31 -0600

    Change email and last name like required fields

--- src/bookborrowing/users/models.py
+++ src/bookborrowing/users/models.py
@@ -61,13 +61,11 @@ class User(AbstractBaseUser, PermissionsMixin, CatalogueMixin):
     email = models.EmailField(
         max_length=255,
         unique=True,
-        blank=True, null=True,
         verbose_name=_('email')
     )
 
     last_name = models.CharField(
         max_length=255,
-        blank=True, null=True,
         verbose_name=_('last name')
     )
 
